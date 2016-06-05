2431de14a29b232836a73d3516d524de30ba4ece
Tipwheal <674714966@qq.com>
2016-06-05 18:06:28 +0800

    16.6.5.yzh.3

--- src/com/tipwheal/dog/Dog.java
+++ src/com/tipwheal/dog/Dog.java
@@ -1,6 +1,7 @@
 package com.tipwheal.dog;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 /**
  * Dog.<br>
@@ -11,33 +12,52 @@ import java.io.Serializable;
  */
 public class Dog implements Serializable {
 	private static final long serialVersionUID = 1L;
-	private int sec;
-	private int min;
-	private int hour;
-	private int day;
-	private int hung = 100;
+	private HashMap<String, String> sFea = new HashMap<>();
+	private HashMap<String, Integer> iFea = new HashMap<>();
+	private HashMap<String, Double> dFea = new HashMap<>();
+
+	public Dog() {
+		sFea.put("name", "Jack");
+		iFea.put("sec", 0);
+		iFea.put("min", 0);
+		iFea.put("hour", 0);
+		iFea.put("day", 0);
+		iFea.put("hung", 100);
+	}
 
 	public void feed() {
-		hung += 50;
+		iFea.replace("hung", iFea.get("hung") + 50);
 	}
 
 	public void grow() {
-		sec++;
-		hung--;
-		min = sec / 60;
-		hour = min / 60;
-		day = hour / 24;
+		iFea.replace("sec", iFea.get("sec") + 10);
+		iFea.replace("min", iFea.get("sec") / 60);
+		iFea.replace("hour", iFea.get("min") / 60);
+		iFea.replace("day", iFea.get("hour") / 24);
+		iFea.replace("hung", iFea.get("hung") - 1);
+	}
+
+	public int getMin() {
+		return iFea.get("min");
 	}
 
 	public int getHour() {
-		return hour;
+		return iFea.get("hour");
 	}
 
 	public int getDay() {
-		return day;
+		return iFea.get("day");
 	}
 
 	public int getHung() {
-		return hung;
+		return iFea.get("hung");
+	}
+
+	public String getName() {
+		return sFea.get("name");
+	}
+
+	public void setName(String name) {
+		sFea.replace("name", name);
 	}
 }
