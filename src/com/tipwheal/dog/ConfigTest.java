9b2559994641069c2453aa4954186aba94109227
Tipwheal <674714966@qq.com>
2016-06-06 13:04:41 +0800

    16.6.6.yzh.1

---
+++ src/com/tipwheal/dog/ConfigTest.java
@@ -0,0 +1,30 @@
+package com.tipwheal.dog;
+
+import java.lang.reflect.Method;
+
+public class ConfigTest {
+	// public static void main(String[] args) {
+	// try {
+	// Runtime.getRuntime().exec("new ConfigTest().show()");
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	// }
+
+	public void show() {
+		System.out.println("right");
+	}
+
+	public static void main(String[] args) throws Exception {
+		Dog dog = new Dog();
+		Method m = new Action().getClass().getDeclaredMethod("show",Dog.class);
+		m.invoke(new Action(),new Object[] {dog});
+		// String methodName = "show";
+		// String params = "value";
+		// ConfigTest obj = new ConfigTest();
+		// Class classType = obj.getClass();
+		// Method method = classType.getDeclaredMethod(methodName, new Class[]
+		// {});
+		// method.invoke(obj, new Object[] {});
+	}
+}
